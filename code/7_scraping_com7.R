50d7edec6f9016d32304e37e2024b8d5ae52686a
simonedeluca <delucasim@hotmail.it>
2023-03-30 17:56:34 +0200

    scraping commissione 7 - pt. 1

---
+++ code/7_scraping_com7.R
@@ -0,0 +1,157 @@
+library("rvest")
+library("dplyr")
+library("stringr")
+
+# 1. Estrazione atto
+
+atto <- vector(mode = "list", length = 33)
+
+for (page_index in 1:33) {
+  link <- paste("https://www.senato.it/Leg18/3661?current_page_40431=",page_index,"", sep = "")
+  page <- read_html(link)
+  x <- page %>% html_nodes(".descrizione em") %>% html_text()
+  atto[[page_index]] <- x
+  print(paste("Page:", page_index))
+  Sys.sleep(5)
+}
+
+x <- atto
+
+x[[3]] <- append(x[[3]], "NA", after = 4)
+
+x[[4]] <- append(x[[4]], "progetti in corso nei siti archeologici italiani per il monitoraggio plurisistemico volto a contrastare l'impatto dei cambiamenti climatici", after = 0)
+
+x[[9]] <- append(x[[9]], "NA", after = 4)
+
+x[[17]] <- append(x[[17]], "NA", after = 4)
+
+x[[20]] <- append(x[[20]], "NA", after = 6)
+x[[20]] <- append(x[[20]], "NA", after = 9)
+
+atto <- x
+
+# 2. Estrazione data
+
+data <- vector(mode = "list", length = 33)
+
+for (page_index in 1:33) {
+  link <- paste("https://www.senato.it/Leg18/3661?current_page_40431=",page_index,"", sep = "")
+  page <- read_html(link)
+  x <- page %>% html_nodes(".data em") %>% html_text()
+  data[[page_index]] <- x
+  print(paste("Page:", page_index))
+  Sys.sleep(5)
+}
+
+a <- data
+
+data_fill <- vector(mode = "list", length = 8)
+
+for (page_index in 26:33) {
+  link <- paste("https://www.senato.it/Leg18/3661?current_page_40431=",page_index,"", sep = "")
+  page <- read_html(link)
+  x <- page %>% html_nodes(".data_ora_inizio a") %>% html_text()
+  data_fill[[page_index]] <- x
+  print(paste("Page:", page_index))
+  Sys.sleep(5)
+}
+
+for (i in 26:33) {
+  data_fill[[i]] <- gsub("del", "", data_fill[[i]]) %>% #togli specific pattern
+    str_trim(side = "both") # togli white space all'inizio e alla fine!
+}
+
+for (i in 26:33) {
+  a[[i]] <- data_fill[[i]]
+}
+
+a[[26]][[1]] <- "26 settembre 2018"
+a[[30]] <- append(a[[30]], "NA", after = 1)
+
+data <- a
+rm(data_fill)
+rm(a)
+
+# 3. Estrazione commissioni coinvolte
+
+commissione <- vector(mode = "list", length = 33)
+
+for (page_index in 1:33) {
+  link <- paste("https://www.senato.it/Leg18/3661?current_page_40431=",page_index,"", sep = "")
+  page <- read_html(link)
+  x <- page %>% html_nodes(".strong") %>% html_text()
+  commissione[[page_index]] <- x
+  print(paste("Page:", page_index))
+  Sys.sleep(5)
+}
+
+x <- commissione
+
+a <- "7 (Pubbl. istruzione)"
+n <- c(1,4,5,6,7,9,13:19,21:25,32)
+
+for (i in n) {
+  x[[i]] <- rep(a, 10)
+}
+
+x[[2]] <- append(x[[2]], a, after = 0)
+x[[2]] <- append(x[[2]], a, after = 8)
+x[[2]] <- append(x[[2]], a, after = 9)
+
+a <- rep("7 (Pubbl. istruzione)", 7)
+x[[3]] <- append(x[[3]], a, after = 0)
+x[[3]] <- append(x[[3]], "7 (Pubbl. istruzione)", after = 9)
+
+a <- rep("7 (Pubbl. istruzione)", 4)
+x[[8]] <- append(x[[8]], a, after = 0)
+x[[8]] <- append(x[[8]], "7 (Pubbl. istruzione)", after = 8)
+
+a <- rep("7 (Pubbl. istruzione)", 4)
+x[[10]] <- append(x[[10]], a, after = 0)
+a <- rep("7 (Pubbl. istruzione)", 5)
+x[[10]] <- append(x[[10]], a, after = 5)
+
+a <- rep("7 (Pubbl. istruzione)", 6)
+x[[11]] <- append(x[[11]], a, after = 0)
+a <- rep("7 (Pubbl. istruzione)", 2)
+x[[11]] <- append(x[[11]], a, after = 7)
+
+a <- rep("7 (Pubbl. istruzione)", 8)
+x[[12]] <- append(x[[12]], a, after = 2)
+
+a <- rep("7 (Pubbl. istruzione)", 5)
+x[[20]] <- append(x[[20]], a, after = 0)
+a <- rep("7 (Pubbl. istruzione)", 4)
+x[[20]] <- append(x[[20]], a, after = 6)
+
+a <- rep("7 (Pubbl. istruzione)", 8)
+x[[26]] <- append(x[[26]], a, after = 0)
+x[[26]] <- append(x[[26]], "7 (Pubbl. istruzione)", after = 9)
+
+x[[27]] <- append(x[[27]], "7 (Pubbl. istruzione)", after = 3)
+x[[27]] <- append(x[[27]], "7 (Pubbl. istruzione)", after = 5)
+x[[27]] <- append(x[[27]], "7 (Pubbl. istruzione)", after = 7)
+x[[27]] <- append(x[[27]], "7 (Pubbl. istruzione)", after = 8)
+
+x[[28]] <- append(x[[28]], "7 (Pubbl. istruzione)", after = 1)
+a <- rep("7 (Pubbl. istruzione)", 4)
+x[[28]] <- append(x[[28]], a, after = 4)
+
+a <- rep("7 (Pubbl. istruzione)", 2)
+x[[29]] <- append(x[[29]], a, after = 0)
+a <- rep("7 (Pubbl. istruzione)", 4)
+x[[29]] <- append(x[[29]], a, after = 3)
+x[[29]] <- append(x[[29]], "7 (Pubbl. istruzione)", after = 8)
+
+a <- rep("7 (Pubbl. istruzione)", 4)
+x[[30]] <- append(x[[30]], a, after = 0)
+a <- rep("7 (Pubbl. istruzione)", 5)
+x[[30]] <- append(x[[30]], a, after = 5)
+
+x[[31]] <- append(x[[31]], "7 (Pubbl. istruzione)", after = 1)
+a <- rep("7 (Pubbl. istruzione)", 3)
+x[[31]] <- append(x[[31]], a, after = 4)
+
+x[[33]][[1]] <- "7 (Pubbl. istruzione)"
+
+commissione <- x
