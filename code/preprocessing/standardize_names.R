5b240c0944071823924f429ea87bf1c98162ea14
simonedeluca <delucasim@hotmail.it>
2023-09-04 19:58:22 +0200

    standardizing names

---
+++ code/preprocessing/standardize_names.R
@@ -0,0 +1,36 @@
+library(rio)
+library(stringr)
+
+setwd("C:/Users/pc/Desktop/notes/Progetto Audizioni")
+camera <- import("Mappatura audizioni informali Camera (definitiva e con PNRR).xlsx")
+
+camera <- camera[-1,]
+
+enti_camera <- camera$`Ente/Organizzazione`
+nomecogn_camera <- camera$`Nome e cognome`
+
+df <- data.frame(
+  enti_camera = enti_camera,
+  nomecogn_camera = nomecogn_camera
+)
+
+df$nomi_camera <- ifelse(is.na(df$enti_camera), nomecogn_camera,
+                         enti_camera)
+
+nomi_camera <- df$nomi_camera
+nomi_camera <- str_to_upper(nomi_camera)
+nomi_camera <- unique(sort(nomi_camera))
+
+setwd("C:/Users/pc/Desktop/Progetto Audizioni")
+senato <- import("data.xlsx")
+
+nomi_senato <- senato$NOMI
+nomi_senato <- nomi_senato[!is.na(nomi_senato)]
+
+nomi_senato <- str_to_upper(nomi_senato)
+nomi_senato <- unique(sort(nomi_senato))
+
+intersect(nomi_camera,nomi_senato) #346 nomenclature uguali
+
+# TASK: standardize the nomenclature of the collected names.
+
