932afe1bcfb619fb4d37503d54042584851cafd8
simonedeluca <delucasim@hotmail.it>
2023-09-20 01:36:27 +0200

    in progress

--- code/preprocessing/standardize_names.R
+++ code/preprocessing/standardize_names.R
@@ -1,25 +1,87 @@
-library(rio)
+library(readxl)
+library(dplyr)
 library(stringr)
 
 setwd("C:/Users/pc/Desktop/notes/Progetto Audizioni")
-camera <- import("Mappatura audizioni informali Camera (definitiva e con PNRR).xlsx")
+camera <- read_excel("Mappatura audizioni informali Camera (definitiva e con PNRR).xlsx", sheet = "Audizioni 2018_2020")
 
 camera <- camera[-1,]
+camera <- camera[,-c(8:12)]
 
-enti_camera <- camera$`Ente/Organizzazione`
-nomecogn_camera <- camera$`Nome e cognome`
+camera$nomi_camera <- ifelse(is.na(camera$`Ente/Organizzazione`), camera$`Nome e cognome`,
+                             camera$`Ente/Organizzazione`)
+sum(is.na(camera$nomi_camera))
 
-df <- data.frame(
-  enti_camera = enti_camera,
-  nomecogn_camera = nomecogn_camera
-)
+camera %>% count(`Tipologia soggetto`)
 
-df$nomi_camera <- ifelse(is.na(df$enti_camera), nomecogn_camera,
-                         enti_camera)
+camera$`Tipologia soggetto` <- ifelse(camera$`Tipologia soggetto`=="Sindacati e associazioni di categorie", "Sindacati e associazioni di categoria",
+                                      ifelse(camera$`Tipologia soggetto`=="Aziende a partecipazione pubblica", "Aziende e società a partecipazione pubblica",
+                                             camera$`Tipologia soggetto`))
 
-nomi_camera <- df$nomi_camera
-nomi_camera <- str_to_upper(nomi_camera)
-nomi_camera <- unique(sort(nomi_camera))
+camera %>% count(`Tipologia soggetto`)
+
+aziende <- camera %>%
+  filter(`Tipologia soggetto`=="Aziende") %>%
+  select(nomi_camera)
+
+table(aziende)
+aziende$nomi_camera <- str_to_upper(aziende$nomi_camera)
+
+aziende$nomi_camera <- ifelse(aziende$nomi_camera=="ARCELORMITTAL ITALIA", "ARCELORMITTAL",
+                              ifelse(aziende$nomi_camera=="ARCELOR MITTAL", "ARCELORMITTAL",
+                                     ifelse(aziende$nomi_camera=="SAMSUNG ELECTONICS SPA", "SAMSUNG ELECTRONICS",
+                                            ifelse(aziende$nomi_camera=="SAMSUNG ELECTRONIC SPA", "SAMSUNG ELECTRONICS",
+                                                   aziende$nomi_camera))))
+
+table(aziende)
+
+to_remove <- "SRL|SPA|S.R.L.|S.P.A.|ITALY|ITALIA"
+aziende$nomi_camera2 <- gsub(to_remove, "", aziende$nomi_camera)
+
+
+
+
+
+
+
+
+camera$nomi_camera <- gsub("\\.", "", camera$nomi_camera)
+
+camera <- str_split_fixed(camera$nomi_camera, ",", 2)
+camera$nomi_camera <- str_to_upper(camera$nomi_camera)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 setwd("C:/Users/pc/Desktop/Progetto Audizioni/data/preprocessed_data")
 senato <- import("dataset_senato.xlsx")
